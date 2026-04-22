982157ad9385eeae67a48175698140f2c14e318d
Luis Antonio Arguello Escutia <luisantoniobros10@gmail.com>
2026-04-21 21:40:24 -0600

    fix: Aplicar modo oscuro de forma global en App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,16 @@
 import { RouterProvider } from 'react-router';
 import { AuthProvider } from './context/AuthContext';
 import { router } from './routes';
+import { useEffect } from 'react';
 
 export default function App() {
+  useEffect(() => {
+    const saved = localStorage.getItem('darkMode');
+    if (saved === 'true') {
+      document.documentElement.classList.add('dark');
+    }
+  }, []);
+
   return (
     <AuthProvider>
       <RouterProvider router={router} />
